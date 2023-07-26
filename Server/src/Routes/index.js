6a8f43229087e1295e9df14719f8e8b9dd2432af
LucasGonzalezRighi <lucasgonzalezrighi@gmail.com>
2023-07-26 12:22:20 -0300

    rickandmorty

---
+++ Server/src/Routes/index.js
@@ -0,0 +1,20 @@
+const {getCharById} = require('../Controllers/getCharById');
+const login = require('../Controllers/login');
+const postUser = require('../Controllers/postUser');
+const postFav = require('../Controllers/postFavs');
+const deleteFav = require('../Controllers/deleteFav');
+
+
+
+const router = require('express').Router();
+
+
+router.get('/login', login );
+router.post('/login', postUser );
+router.post('/fav', postFav);
+router.delete('/fav/:id', deleteFav);
+router.get('/character/:id', getCharById);
+
+
+
+module.exports = router;
